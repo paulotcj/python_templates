18abcd463e2272cb2a3d500c543cdb22d1ec1ac7
Paulo Costa <paulotcj@gmail.com>
2023-02-11 03:09:05 -0500

    .

--- archive/tkinter/tkinter4.py
+++ archive/tkinter/tkinter4.py
@@ -8,11 +8,10 @@ window.minsize(width = 500, height = 300)
 
 #Label 1
 label1 = tkinter.Label(text="I am a label",font=("Arial", 24, "italic"))
-
-#columns and rows are in reference to others. So if we were to define this as column 10 but no
-# other item is referencing the grid system, this item would be placed at the leftmost place in the screen
-label1.grid(column=0, row=0) 
-
+label1.place(x=0,y=50)
+# label1.pack(side = "left")
+# label1.pack(side = "bottom")
+# label1.pack(expand = True)
 
 label1["text"] = "I am a label - text modified"
 
@@ -23,13 +22,12 @@ def button1_clicked():
     label1["text"] = input1.get()
 
 button1 = tkinter.Button(text="click here", font=("Arial", 14), command=button1_clicked)
-# button1.grid(column=5, row=1)  -> column 5 - it doesnt work
-button1.grid(column=1, row=1) 
+button1.pack(side="left")
 
 
 #input / entry
 input1 = tkinter.Entry(width=30,font=("Arial", 14))
-input1.grid(column=2, row=2) 
+input1.pack(side="left")
 
 
 
