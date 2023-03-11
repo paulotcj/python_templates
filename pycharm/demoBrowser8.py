fdfd68a270e0c11df64667be0d098fe6edba0258
Paulo Costa <paulotcj@gmail.com>
2023-03-11 13:57:41 -0500

    .

---
+++ pycharm/demoBrowser8.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver import ActionChains
+
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+url = "https://www.smashingmagazine.com/2017/05/long-scrolling/"
+driver.get(url)
+print(driver.current_url)
+# specify the amount of time the WebDriver instance i.e. the driver should wait when searching for an element if it is not immediately present in the HTML DOM
+# driver.implicitly_wait(5)
+
+# --------------------
+driver.execute_script("window.scrollBy(0,1000);")
+driver.get_screenshot_as_file("screenshots/1.png")
+time.sleep(2)
+driver.execute_script("window.scrollBy(0,1000);")
+driver.get_screenshot_as_file("screenshots/2.png")
+time.sleep(2)
+driver.execute_script("window.scrollBy(0,1000);")
+driver.get_screenshot_as_file("screenshots/3.png")
+time.sleep(2)
+driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+driver.get_screenshot_as_file("screenshots/4.png")
+# --------------------
+
+time.sleep(7)
+driver.close()
