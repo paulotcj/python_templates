6c8311bd0a05b88546876965c1ca7889dcc1fe3b
Paulo Costa <paulotcj@gmail.com>
2023-03-11 05:38:15 -0500

    .

--- pycharm/demoBrowser4.py
+++ pycharm/demoBrowser4.py
@@ -12,14 +12,17 @@ url = "https://www.selenium.dev/documentation/webdriver/interactions/alerts/"
 driver.get(url)
 print(driver.current_url)
 
+# specify the amount of time the WebDriver instance i.e. the driver should wait when searching for an element if it is not immediately present in the HTML DOM
+driver.implicitly_wait(5)
 # --------------------
 # Click the link to activate the alert
 driver.find_element(By.LINK_TEXT, "See an example alert").click()
 
-WebDriverWait(driver, 10).until(expected_conditions.alert_is_present())
+wait = WebDriverWait(driver, 10)
+wait.until(expected_conditions.alert_is_present())
 
 alert = driver.switch_to.alert
-# print(alert.text)
+print("Alert text:",alert.text)
 
 alert.accept()
 
