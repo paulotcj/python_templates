0125332e06fee58b7e1f869c9fbb28caca2da2e5
Paulo Costa <paulotcj@gmail.com>
2023-03-11 12:28:52 -0500

    .

--- pycharm/demoBrowser.py
+++ pycharm/demoBrowser.py
@@ -3,16 +3,22 @@ from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver import ActionChains
+
 
 driver = webdriver.Chrome()
 driver.maximize_window()
-url = "https://www.selenium.dev/documentation/webdriver/interactions/alerts/"
+url = "https://www.selenium.dev/selenium/web/mouse_interaction.html"
 driver.get(url)
 print(driver.current_url)
 
 # specify the amount of time the WebDriver instance i.e. the driver should wait when searching for an element if it is not immediately present in the HTML DOM
 driver.implicitly_wait(5)
 # --------------------
+action = ActionChains(driver)
+action.move_to_element( driver.find_element(By.ID, "hover") ).perform()
+time.sleep(5)
+action.drag_and_drop( driver.find_element(By.ID, "draggable"), driver.find_element(By.ID, "droppable") ).perform()
 
 
 # --------------------
@@ -20,5 +26,5 @@ driver.implicitly_wait(5)
 
 
 
-time.sleep(3)
+time.sleep(7)
 driver.close()
