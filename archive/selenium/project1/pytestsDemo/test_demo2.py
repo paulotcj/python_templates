e6b06acb0fc556a308435b5d528906e079d855bc
Paulo Costa <paulotcj@gmail.com>
2023-03-13 14:40:55 -0400

    .

---
+++ archive/selenium/project1/pytestsDemo/test_demo2.py
@@ -0,0 +1,19 @@
+#pytest files should start with 'test_' or end with 'test_'
+#pytest methods should start with 'test_'
+
+def test_firstProgram():
+    print("Hello world")
+
+    assert 1==1
+
+def test_Tests_01():
+    msg = "Hello"
+
+    expected = "Hello"
+    assert msg == expected , f"Test failed, expected:{expected} but received:{msg}"
+
+def test_Tests_02():
+    msg = "Hello"
+
+    expected = "Hello World"
+    assert msg == expected , f"Custom Message - Test failed, expected:{expected} but received:{msg}"
