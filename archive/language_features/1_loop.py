832789af47638e616d208cf7027a5a03760e6650
Paulo Costa <paulotcj@gmail.com>
2023-12-23 18:17:35 -0500

    .

--- archive/language_features/1_loop.py
+++ archive/language_features/1_loop.py
@@ -1,4 +1,19 @@
 from datetime import datetime
+from numba import njit
+#------------------------------------------------------------
+def func_test():
+    t1 = datetime.now()
+    n = 0
+    for _ in range(1_000_000_000):
+        n += 1
+
+    print(n)
+    t2 = datetime.now()
+    t_delta = t2 - t1
+    print(f"time delta:{t_delta}")    
+
+#------------------------------------------------------------
+#------------------------------------------------------------
 t1 = datetime.now()
 n = 0
 for _ in range(1_000_000_000):
@@ -8,3 +23,12 @@ print(n)
 t2 = datetime.now()
 t_delta = t2 - t1
 print(f"time delta:{t_delta}")
+#------------------------------------------------------------
+n=0
+t1 = datetime.now()
+[ i for i in range(1_000_000_000)]
+t2 = datetime.now()
+print(n)
+t_delta = t2 - t1
+print(f"list compreension time delta:{t_delta}")
+#------------------------------------------------------------
