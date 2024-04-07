be74a9da59fd5bafa8cc29ad8a577f416b1c1398
Paulo <paulotcj@gmail.com>
2024-04-07 11:26:46 -0400

    .

--- numpy/2_test.py
+++ numpy/2_test.py
@@ -113,3 +113,35 @@ print(arr)
 print('-----')
 arr = np.concatenate((arr1, arr2), axis=0)
 print(arr)
+
+
+print('----------------------------------------------')
+
+
+arr1 = np.array([1, 2, 3])
+
+arr2 = np.array([4, 5, 6])
+
+arr = np.stack((arr1, arr2), axis = 1)
+print(arr)
+print('-----')
+arr = np.stack((arr1, arr2), axis = 0)
+print(arr)
+
+print('----------------------------------------------')
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+
+arr = np.hstack((arr1,arr2))
+
+print(arr)
+print('----------------------------------------------')
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+
+arr = np.vstack((arr1, arr2))
+
+
+print(arr)
