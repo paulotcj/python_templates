8ac21b4ba9bc5d762b135cefc9d16e22855d8023
Paulo Costa <paulotcj@gmail.com>
2024-04-07 21:38:10 -0400

    .

--- numpy/2_test.py
+++ numpy/2_test.py
@@ -177,3 +177,125 @@ print(arr1)
 print(arr1[0])
 print(arr1[1])
 print(arr1[2])
+
+
+print('----------------------------------------------')
+arr = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]])
+print(f'num dimensions of arr: {arr.ndim}')
+newarr = np.array_split(arr, 3)
+for x in newarr:
+    print(x)
+    print('-----')
+
+
+print('----------------------------------------------')
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]])
+
+newarr = np.array_split(arr, 3)
+
+for x in newarr:
+    print(x)
+    print('-----')
+
+print('----------------------------------------------')
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]])
+newarr = np.array_split(arr, 3, axis=1)
+for x in newarr:
+    print(x)
+    print('-----')
+
+print('----------------------------------------------')
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]])
+newarr = np.hsplit(arr, 3)
+for x in newarr:
+    print(x)
+    print('-----')
+
+print('----------------------------------------------')
+
+#               0  1  2  3  4  5  6
+arr = np.array([1, 2, 3, 4, 5, 4, 4])
+
+result = np.where(arr == 4)
+print(type(result))
+print(result)
+print('-----')
+print('each of the results below should be 4')
+for x in result: #x is a tuple containing an array with the indexes where arr == 4
+    for y in x: #y is the actual element in the array
+        print(arr[y])
+
+print('----------------------------------------------')
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+result = np.where(arr%2 == 0)
+print(result)
+for x in result:
+    for y in x:
+        print(arr[y])
+
+print('----------------------------------------------')
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+result = np.where(arr%2 == 1)
+print(result)
+for x in result: 
+    for y in x: print(arr[y])
+
+print('----------------------------------------------')
+
+#               0  1  2  3  
+arr = np.array([6, 7, 8, 9])
+
+x = np.searchsorted(arr, 7)
+print(x)
+
+x = np.searchsorted(arr, 7.5)
+print(x)
+
+x = np.searchsorted(arr, 8)
+print(x)
+
+
+x = np.searchsorted(arr, 8.5)
+print(x)
+
+print('----------------------------------------------')
+
+arr = np.array([6, 7, 8, 9])
+
+x = np.searchsorted(arr, 7)
+print(x)
+
+x = np.searchsorted(arr, 7, side='right')
+print(x)
+
+print('----------------------------------------------')
+
+#               0  1  2  3
+arr = np.array([1, 3, 5, 7])
+
+x = np.searchsorted(arr, [2, 4, 6])
+
+print(x)
+
+
+print('----------------------------------------------')
+
+arr = np.array([3, 2, 0, 1])
+sorted_asc = np.sort(arr)
+sorted_desc = np.sort(arr)[::-1]
+print(arr)
+print(sorted_asc)
+print(sorted_desc)
+
+print('----------------------------------------------')
+arr = np.array(['banana', 'cherry', 'apple', 'aa', 'a', 'aab'])
+
+print(np.sort(arr))
+
+print('----------------------------------------------')
+
+arr = np.array([True, False, True]) #false can be considered 0 and true = 1, so we should see False, True, True
+
+print(np.sort(arr))
