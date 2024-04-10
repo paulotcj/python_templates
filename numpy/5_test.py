45821b7ac26847713e60cb77ff2c4f955d1bc390
Paulo <Costa>
2024-04-09 20:36:54 -0400

    .

--- numpy/5_test.py
+++ numpy/5_test.py
@@ -148,6 +148,202 @@ print('----------------------------------------------')
 arr = np.ceil([-3.1666, 3.6667]) #the upper/ceil value of -3.166 is -3, and for 3.6667 is 4
 print(arr)
 
+print('----------------------------------------------')
+arr1 = np.array([1, 2, 3, 1])
+arr2 = np.array([1, 2, 3, 2])
+
+newarr = np.sum([arr1, arr2], axis=1) # 7,8
+print(newarr)
+newarr = np.sum([arr1, arr2], axis=0) # 2,4, 6, 3
+print(newarr)
+newarr = np.sum([arr1, arr2]) #7+8 = 15
+print(newarr)
+
+print('----------------------------------------------')
+arr = np.array([1,1,1])
+
+newarr = np.cumsum(arr)
+
+print(newarr)
+
+print('----------------------------------------------')
+arr = np.array([1, 2, 3])
+newarr = np.cumsum(arr)
+print(newarr)
+
+print('----------------------------------------------')
+arr = np.array([2,5,2]) # 2*5*2 = 20
+
+x = np.prod(arr)
+
+print(x)
+
+print('----------------------------------------------')
+arr1 = np.array([1, 2, 3, 4])
+arr2 = np.array([5, 6, 7, 8])
+
+x = np.prod([arr1, arr2]) # 1*2*3*4*5*6*7*8 = 40320
+
+print(x)
+
+print('----------------------------------------------')
+arr1 = np.array([1, 2, 3, 4])
+arr2 = np.array([5, 6, 7, 8])
+
+x = np.prod(arr1)
+print(x)
+x = np.prod(arr2)
+print(x)
+newarr = np.prod([arr1, arr2], axis=1) #multiply arr1 and store its value, multiply arr2 and store its value
+
+print(newarr)
+
+print('----------------------------------------------')
+
+arr = np.array([5, 6, 7, 8]) # 5, 5*6 = 30, 5*6*7 = 210, 5*6*7*8 = 1680
+
+newarr = np.cumprod(arr)
+
+print(newarr)
+
+print('----------------------------------------------')
+arr = np.array([1, 2, 3, 4])
+
+newarr = np.diff(arr) #[2-1, 3-2, 4-3] = [1, 1, 1]
+
+print(newarr)
+
+print('----------------------------------------------')
+
+num1 = 4
+num2 = 6
+
+x = np.lcm(num1, num2)
+
+print(x)
+
+print('----------------------------------------------')
+arr = np.array([6,4])
+
+x = np.lcm.reduce(arr)
+
+print(x)
+
+print('----------------------------------------------')
+
+
+num1 = 6
+num2 = 9
+
+x = np.gcd(num1, num2)
+
+print(x)
+
+print('----------------------------------------------')
+
+
+arr = np.array([20, 8, 32, 36, 16])
+
+x = np.gcd.reduce(arr)
+
+print(x)
+
+print('----------------------------------------------')
+
+x = np.sin(np.pi/2)
+
+print(x)
+x = np.round( np.cos(np.pi/2), 4)
+
+print(x)
+
+
+print('----------------------------------------------')
+arr = np.array([np.pi/2, np.pi/3, np.pi/4, np.pi/5])
+
+x = np.sin(arr)
+
+print(x)
+
+print('----------------------------------------------')
+arr = np.array([90, 180, 270, 360])
+
+x = np.deg2rad(arr)
+
+print(x)
+
+
+print('----------------------------------------------')
+arr = np.array([np.pi/2, np.pi, 1.5*np.pi, 2*np.pi])
+
+x = np.rad2deg(arr)
+print(x)
+print('----------------------------------------------')
+x = np.arcsin(1.0)
+
+print(x)
+
+
+print('----------------------------------------------')
+arr = np.array([1, -1, 0.1])
+
+x = np.arcsin(arr)
+
+print(x)
+
+
+print('----------------------------------------------')
+base = 3
+perp = 4
+
+x = np.hypot(base, perp)
+
+print(x)
+
+print('----------------------------------------------')
+
+
+arr = np.array([1, 1, 1, 2, 3, 4, 5, 5, 6, 7])
+
+x = np.unique(arr)
+
+print(x)
+
+print('----------------------------------------------')
+arr1 = np.array([1, 2, 3, 4, 7])
+arr2 = np.array([3, 4, 5, 6])
+
+newarr = np.union1d(arr1, arr2)
+
+print(newarr)
+
+print('----------------------------------------------')
+arr1 = np.array([1, 2, 3, 4])
+arr2 = np.array([3, 4, 5, 6]) #3 and 4 repeat
+
+newarr = np.intersect1d(arr1, arr2, assume_unique=True)
+
+print(newarr)
+
+print('----------------------------------------------')
+set1 = np.array([1, 2, 3, 4])
+set2 = np.array([3, 4, 5, 6])
+
+newarr = np.setdiff1d(set1, set2, assume_unique=True) #from array 1 the only ones unique are 1 and 2
+
+print(newarr)
+
+print('----------------------------------------------')
+
+#Symmetric Difference
+set1 = np.array([1, 2, 3, 4]) #1 and 2 are unique
+set2 = np.array([3, 4, 5, 6]) # 5 and 6 are unique
+
+newarr = np.setxor1d(set1, set2, assume_unique=True)
+
+print(newarr)
+
+
 
 
 
