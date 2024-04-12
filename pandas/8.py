d2b59c7eb94c9b249b35157428540504d155cfcb
Paulo <Costa>
2024-04-11 21:22:28 -0400

    .

--- pandas/8.py
+++ pandas/8.py
@@ -1,2 +1,39 @@
 import pandas as pd
 
+df = pd.read_csv('data2.csv')
+
+df.loc[7, 'Duration'] = 45
+print(df.to_string())
+
+print('----------------------------------------------')
+
+df = pd.read_csv('data2.csv')
+
+for i, v in df.iterrows():
+    print(f'i: {i}, v[\"Duration\"]: {v["Duration"]}')
+    
+    
+print('----------------------------------------------')
+
+df = pd.read_csv('data2.csv')
+for x in df.index:
+    if df.loc[x, "Duration"] >= 121:
+        df.loc[x, "Duration"] = 120
+        
+print(df.to_string())
+
+print('----------------------------------------------')
+
+df = pd.read_csv('data2.csv')
+for x in df.index:
+    if df.loc[x, "Duration"] >= 121:
+        df.drop(x, inplace = True)
+        
+print(df.to_string())
+
+
+    
+
+
+ 
+ 
