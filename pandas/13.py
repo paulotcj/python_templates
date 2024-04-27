30150426d0cedd7b594332128283192713392a74
Paulo <paulotcj@gmail.com>
2024-04-27 00:46:25 -0400

    .

--- pandas/13.py
+++ pandas/13.py
@@ -46,3 +46,73 @@ print(transactions.describe(include="all"))
 print('----------------------------------------------')
 print('skip the first line and get only store_nbr and transactions columns')
 print(transactions.loc[1:,"store_nbr":"transactions"])
+
+print('----------------------------------------------')
+
+print(transactions.loc[:, "store_nbr"].nunique())
+
+
+print('----------------------------------------------')
+print('drop the first row of data in place')
+transactions.drop(0, axis=0, inplace=True)
+print(transactions)
+
+print('----------------------------------------------')
+print('drop date but not in place')
+print(transactions.drop("date", axis=1))
+print('----')
+print(transactions)
+
+print('----------------------------------------------')
+print('drop duplicate rows subsetting by store number. keep last entry for each store')
+
+print(transactions.drop_duplicates(subset="store_nbr", keep="last").head())
+
+print('----------------------------------------------')
+
+oil = pd.read_csv("./oil.csv")
+
+print(oil)
+print('----')
+print(oil.info())
+print('----')
+print(oil.isna().sum())
+
+print('----------------------------------------------')
+print('calculate mean of oil series after filling missing values with 0')
+print(oil.loc[:, 'dcoilwtico'].fillna(0).mean().round(2))
+
+
+print('calculate mean of oil series after filling missing values with the mean of oil price')
+
+print(
+        oil.loc[:, 'dcoilwtico'].fillna(
+            oil.loc[:, 'dcoilwtico'].mean()
+        ).mean().round(2)
+    )
+
+print('----------------------------------------------')
+print('all stores > 2500 percentage occurence')
+
+print( (transactions['transactions'] > 2000).mean() )
+
+print('----------------------------------------------')
+print('Number of times store 25 had > 2000 divided by total days for store 25 to get percent of time it happened')
+
+mask = (transactions['transactions'] > 2000) & (transactions['store_nbr'] == 25)
+
+x = (transactions.loc[mask, 'transactions'].count() 
+ / transactions.loc[(transactions['store_nbr'] == 25), 'transactions'].count())
+print(x)
+
+print('----------------------------------------------')
+print('Sum of transactions where store 25 had > 2000 transactions')
+print(transactions.loc[mask, 'transactions'].sum())
+
+print('----------------------------------------------')
+print('sum of transactions for stores 25 and 31 in months May and June on days they had less than 2000 transactions')
+x = (transactions.query(
+    "store_nbr in [25, 31] & date.str[6] in ['5', '6'] & transactions < 2000")
+    .loc[:, "transactions"]
+    .sum())
+print(x)
